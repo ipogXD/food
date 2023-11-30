ac6d945219a68c59cf150ed39437ff6fafa5670a
iPogXD <ipoghosyan@mail.ru>
2023-11-30 19:25:57 +0400

    final 40

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import {RatingModule} from 'ng-starrating';
 import { SearchComponent } from './search/search.component';
 import {FormsModule} from '@angular/forms';
 import { TagsComponent } from './tags/tags.component';
@@ -27,7 +26,6 @@ import { NotFoundComponent } from './not-found/not-found.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RatingModule,
     FormsModule
   ],
   providers: [],
